6b667b7758799120bf5e229605a05cdc3511711e
Erwin Sulistiono <sulistiono.erwin@gmail.com>
2021-05-17 11:32:34 +0700

    hide config.js mongodb database

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: `mongodb://admin1:P3tWsLiZKaRdcU5@cluster0-shard-00-00.u6qlk.mongodb.net:27017,cluster0-shard-00-01.u6qlk.mongodb.net:27017,cluster0-shard-00-02.u6qlk.mongodb.net:27017/hospitalqueue?ssl=true&replicaSet=atlas-k2xuhs-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  url: `mongodb://admin1:P3tWsLiZKaRdcU5@cluster0-shard-00-00.u6qlk.mongodb.net:27017,cluster0-shard-00-01.u6qlk.mongodb.net:27017,{url}:27017/{databaseName}?ssl=true&replicaSet=atlas-k2xuhs-shard-0&authSource=admin&retryWrites=true&w=majority`,
 };
